02a17ad612165a491977ea4ce27f5b40a5b272da
hermanlyx <hermanlyx@gmail.com>
2023-01-16 00:24:50 +0800

    added chakra provider to story to show style (chakraui storybook addon did not auto wrap)

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,26 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@chakra-ui/storybook-addon',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+    emotionAlias: false,
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+
+    return config;
+  },
+};
--- src/components/FortunePicker.stories.tsx
+++ src/components/FortunePicker.stories.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { FortunePicker } from './FortunePicker';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export default {
   title: 'FortunePicker',
   component: FortunePicker,
 } as ComponentMeta<typeof FortunePicker>;
 
-export const Primary = () => <FortunePicker onClick={() => alert('test')} />;
+export const Primary: ComponentStory<typeof FortunePicker> = () => (
+  <ChakraProvider>
+    <FortunePicker onClick={() => alert('test')} />
+  </ChakraProvider>
+);
