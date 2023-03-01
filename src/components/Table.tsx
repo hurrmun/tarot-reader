dd18ecc145b05ea652c868d6dd833f7482f76401
hermanlyx <hermanlyx@gmail.com>
2023-03-01 22:57:03 +0800

    added chance for card meaning to be reversed, show orientation

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {
   HStack,
@@ -19,22 +19,46 @@ export const Table = ({ selectedCards }: TableProps) => {
   return (
     <HStack justifyContent='center' spacing={16} marginTop={16}>
       {selectedCards.map((card) => {
-        return (
-          <ChakraCard key={card.title} width='100%'>
-            <CardHeader paddingBottom={2}>
-              <Heading size='md'>{card.title}</Heading>
-            </CardHeader>
-            <CardBody>
-              <Text>
-                <Text as='b'>Arcana:</Text> {card.arcana}
-              </Text>
-              <Text>
-                <Text as='b'>Meaning:</Text> {card.description.upright}
-              </Text>
-            </CardBody>
-          </ChakraCard>
-        );
+        return <SelectedCard card={card} />;
       })}
     </HStack>
   );
 };
+
+interface SelectedCardProps {
+  card: Card;
+}
+
+const SelectedCard = ({ card }: SelectedCardProps) => {
+  const [isReversed, setIsReversed] = useState(false);
+
+  useEffect(() => {
+    if (coinFlip()) {
+      setIsReversed(true);
+    }
+  }, []);
+
+  return (
+    <ChakraCard key={card.title} width='100%'>
+      <CardHeader paddingBottom={2}>
+        <Heading size='md'>{card.title}</Heading>
+      </CardHeader>
+      <CardBody>
+        <Text>
+          <Text as='b'>Arcana:</Text> {card.arcana}
+        </Text>
+        <Text>
+          <Text as='b'>Orientation:</Text> {isReversed ? 'Reversed' : 'Upright'}
+        </Text>
+        <Text>
+          <Text as='b'>Meaning:</Text>{' '}
+          {isReversed ? card.description.reversed : card.description.upright}
+        </Text>
+      </CardBody>
+    </ChakraCard>
+  );
+};
+
+function coinFlip() {
+  return Math.floor(Math.random() * 2);
+}
